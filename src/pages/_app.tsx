832b3cc3a11471acce7a44a7e404e8d4a6a3aec9
TiagoDiass <tiago.costadiasss@gmail.com>
2021-12-10 22:42:38 -0300

    feat: add images and change viewport tag to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,13 +9,14 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Next.js Boilerplate</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <link rel='shortcut icon' href='/img/icon-512.png' />
         <link rel='apple-touch-icon' href='/img/icon-512.png' />
         <meta
           name='description'
           content='A boilerplate to work with Next.js, Typescript and more.'
         />
+        <title>Next.js Boilerplate</title>
       </Head>
 
       <GlobalStyles />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,7 +39,6 @@ export default class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&family=Lexend:wght@300;500;600&display=swap'
             rel='stylesheet'
           />
-          <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         </Head>
         <body>
           <Main />
