2a60e33992d3e54670b7f16f39255379b952859c
TiagoDiass <tiago.costadiasss@gmail.com>
2021-12-10 23:00:16 -0300

    feat: add Main UI jest-styled-components

--- src/components/Main/Main.test.tsx
+++ src/components/Main/Main.test.tsx
@@ -2,10 +2,10 @@ import Main from './Main';
 import { render, screen } from '@testing-library/react';
 
 describe('Component: Main', () => {
-  it('should render the heading', () => {
+  it('should render correctly', () => {
     const { container } = render(<Main />);
 
-    expect(screen.getByRole('heading', { name: 'React Avançado' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Next.js Boilerplate' })).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
   });
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,17 @@
+import * as S from './Main.styles';
+
+/**
+ * Component that will have the content of the boilerplate
+ */
 export default function Main() {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo src='/img/logo.svg' alt='Atom image with "React Avançado" next to it.' />
+      <S.Title>Next.js Boilerplate</S.Title>
+      <S.Description>
+        TypesScript, ReactJS, Next.js, Styled Components & <span className='bold'>More!</span>
+      </S.Description>
+      <S.Illustration src='/img/hero-illustration.svg' alt='A developer using a computer' />
+    </S.Wrapper>
   );
 }
