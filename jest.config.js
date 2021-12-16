36ed5f7436d5ac1a582ef2afafc48c27d8a1bdcc
TiagoDiass <tiago.costadiasss@gmail.com>
2021-12-15 22:30:31 -0300

    fix: fix coverage config jest

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,6 @@ module.exports = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/*.stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 };
