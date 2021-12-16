651f22794d43ddd0c01480288a36d09dffc170a1
TiagoDiass <tiago.costadiasss@gmail.com>
2021-12-15 22:24:12 -0300

    feat: add PWA configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <link rel='shortcut icon' href='/img/icon-512.png' />
         <link rel='apple-touch-icon' href='/img/icon-512.png' />
+        <link rel='manifest' href='manifest.json' />
         <meta
           name='description'
           content='A boilerplate to work with Next.js, Typescript and more.'
